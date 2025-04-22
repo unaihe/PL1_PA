66d2a2856ed64cc4a93f701cb83e95360f6558d1
unaihe <u.herrero@edu.uah.es>
2025-04-22 17:15:19 +0200

    Proyecto

---
+++ PL1_Lucas_Unai/src/main/java/poo/pl1_lucas_unai/PL1_Lucas_Unai.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pl1_lucas_unai;
+
+/**
+ *
+ * @author unaih
+ */
+public class PL1_Lucas_Unai {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
