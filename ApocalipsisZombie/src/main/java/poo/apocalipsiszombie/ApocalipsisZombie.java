9c573c44b1d8adb100f4095ed31b5257a88dea5a
unaihe <u.herrero@edu.uah.es>
2025-04-22 17:17:13 +0200

    Ahora si

---
+++ ApocalipsisZombie/src/main/java/poo/apocalipsiszombie/ApocalipsisZombie.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.apocalipsiszombie;
+
+/**
+ *
+ * @author unaih
+ */
+public class ApocalipsisZombie {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
