f7ebae00b34228bce66e8279956315515010c443
unaihe <u.herrero@edu.uah.es>
2025-04-24 12:42:26 +0200

    humano Completo

--- ApocalipsisZombie/src/main/java/poo/apocalipsiszombie/zonas/Comedor.java
+++ ApocalipsisZombie/src/main/java/poo/apocalipsiszombie/zonas/Comedor.java
@@ -4,26 +4,34 @@
  */
 package poo.apocalipsiszombie.zonas;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author unaih
  */
 public class Comedor extends Zona{
     private int nComida=0;
-    
+    private final Semaphore semComida;
+
     //Crear semaforo de contador
     public Comedor() {
+        this.semComida = new Semaphore(0,true);
     }
-    public void dejarComida(){
-        nComida+=2;
-        signalAll();
+    public synchronized void dejarComida() {
+        nComida += 2;
+        semComida.release(2); // Libera 2 permisos (2 unidades de comida)
+        notifyAll(); // Notifica a los hilos que esperan comida
     }
-    public void cogerComida(){
-        if(nComida==0){
-            nComida-=1;}
-        else{
-        
+    public void cogerComida() throws InterruptedException {
+        semComida.acquire(); // Intenta adquirir 1 permiso (1 unidad de comida)
+        synchronized (this) {
+            nComida--;
         }
+    }
+    
+    public synchronized int getCantidadComida() {
+        return nComida;
     }
     
 }
